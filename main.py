1a8526c51bc7b1d23eebd75ded97a8a2a9eae33d
KuoLiChia_windows <richardkuo1999@gmail.com>
2022-09-03 15:52:29 +0800

    Done HDR to 16,8bit

--- main.py
+++ main.py
@@ -2,32 +2,26 @@ import os
 import numpy as np
 import cv2
 from tqdm import tqdm
-from utils.util import histogram_data, loadHdr, tonemapping
+from utils.util import histogram_data, tonemapping, get_dynamic_range
 
 
 dataset = 'HDR-Eye'
 input_path = f'./dataset/{dataset}/'
 save_path = f'./result/{dataset}/'
 draw_histogram_image = True
-GT_to_bit = '16bit'
-
+GT_to_bit = '8bit'
+tonemapping_method = 'Reinhard'
 os.mkdir
 
 
 def get_histogram_data(image_path, result_save_path, bit, draw_histogram_image):
-    if bit == '8bit':
-        dynamic_range = 255
-    elif bit == '16bit':
-        dynamic_range = 65536
 
+    dynamic_range, _ = get_dynamic_range(bit)
     histogram_data(image_path, result_save_path,
                    dynamic_range, draw_histogram_image)
 
 
 if __name__ == '__main__':
-
-    if not os.path.isdir(save_path):
-        os.makedirs(save_path)
 
     # Input data
     print('Input')
@@ -40,6 +34,7 @@ if __name__ == '__main__':
 
     for filename in tqdm(os.listdir(data_path)):
 
+        # get histogram data
         image_path = data_path + filename
         result_save_name = result_save_path+filename[:-4]
         get_histogram_data(image_path, result_save_name,
@@ -47,20 +42,28 @@ if __name__ == '__main__':
 
     print('GT')
 
+    # path
     data_path = input_path+dataset+'-gt/'
-    result_save_path = save_path+dataset+'-gt/'
 
+    result_save_path = save_path+dataset+'-gt/histogram/'
+    tonemapping_save_path = save_path+dataset+'-gt/tonemapping/'
+
+    # check dir
     if not os.path.isdir(result_save_path):
         os.makedirs(result_save_path)
+    if not os.path.isdir(tonemapping_save_path):
+        os.makedirs(tonemapping_save_path)
 
     for filename in tqdm(os.listdir(data_path)):
         image_path = data_path + filename
+        tonemapping_save_name = tonemapping_save_path+filename[:-4]
+
+        # tonemapping
+        imBatch = tonemapping(image_path, GT_to_bit, tonemapping_method)
+        tonemapping_save_name += ".png"
+        cv2.imwrite(tonemapping_save_name, imBatch)
+
+        # get histogram data
         result_save_name = result_save_path+filename[:-4]
-
-        get_histogram_data(image_path, result_save_name,
-                           '8bit', draw_histogram_image)
-        #         imBatch = loadHdr(input_path+i)
-        #         cv2.imwrite(save_path+"image/"+i[:-4] + ".png", imBatch)
-
-        #         load_data(save_path+"image/"+i[:-4] +
-        #                   ".png", save_path+"result/"+i[:-4])
+        get_histogram_data(tonemapping_save_name, result_save_name,
+                           GT_to_bit, draw_histogram_image)
--- utils/util.py
+++ utils/util.py
@@ -3,6 +3,16 @@ import cv2
 from matplotlib import pyplot as plt
 
 
+def get_dynamic_range(bit):
+    if bit == '8bit':
+        dynamic_range = 255
+        astype = 'uint8'
+    elif bit == '16bit':
+        dynamic_range = 65536
+        astype = 'uint16'
+    return dynamic_range, astype
+
+
 def histogram_data(image_path, save_path, dynamic_range, draw_histogram_image):
 
     # Input color image
@@ -29,46 +39,23 @@ def histogram_data(image_path, save_path, dynamic_range, draw_histogram_image):
     np.savetxt(save_path+".txt", hist, fmt='%d', newline=' ')
 
 
-def loadHdr(imName):
+def tonemapping(imName, GT_to_bit, method='Reinhard'):
+
+    dynamic_range, astype = get_dynamic_range(GT_to_bit)
     img = cv2.imread(imName, flags=cv2.IMREAD_ANYDEPTH)
 
-    tonemapDurand = cv2.createTonemapReinhard(2.2, 0, 0, 0)
-
-    ldrDurand = tonemapDurand.process(img)
-
-    im2_16bit = np.clip(ldrDurand * 65536, 0, 65536).astype('uint16')
-    # 255 65536 4294967296
-    return im2_16bit
-
-
-def tonemapping(img, dynamic_range, save_path, color_space, tag=None, method='Reinhard'):
-    # float 32 bits 3 channels
-    img = (img/(dynamic_range-1)).astype('float32')  # 0~1
-
-    if img.ndim != 3:
-        img = cv2.merge([img, img, img])
     if method == 'Reinhard':
-        tonemapReinhard = cv2.createTonemapReinhard(1.5, 0, 0, 0)
-        ldrReinhard = tonemapReinhard.process(img)
-        cv2.imwrite(save_path + tag + color_space +
-                    "_TMresult.png", ldrReinhard * 255)
+        tonemap = cv2.createTonemapReinhard(1.5, 0, 0, 0)
     elif method == 'Drago':
-        tonemapDrago = cv2.createTonemapDrago(1.0, 0.7)
-        ldrDrago = tonemapDrago.process(img)
-        ldrDrago = 3 * ldrDrago
-        cv2.imwrite(save_path + tag + color_space +
-                    "_TMresult.png", ldrDrago * 255)
+        tonemap = cv2.createTonemapDrago(1.0, 0.7)
     elif method == 'Durand':
-        tonemapDurand = cv2.createTonemapDurand(1.5, 4, 1.0, 1, 1)
-        ldrDurand = tonemapDurand.process(img)
-        ldrDurand = 3 * ldrDurand
-        cv2.imwrite(save_path + tag + color_space +
-                    "_TMresult.png", ldrDurand * 255)
+        tonemap = cv2.createTonemapDurand(1.5, 4, 1.0, 1, 1)
     elif method == 'Mantiuk':
-        tonemapMantiuk = cv2.createTonemapMantiuk(2.2, 0.85, 1.2)
-        ldrMantiuk = tonemapMantiuk.process(img)
-        ldrMantiuk = 3 * ldrMantiuk
-        cv2.imwrite(save_path + tag + color_space +
-                    "_TMresult.png", ldrMantiuk * 255)
+        tonemap = cv2.createTonemapMantiuk(2.2, 0.85, 1.2)
     else:
         print("Please enter the tonemapping method!")
+
+    ldr = tonemap.process(img)
+    im2 = np.clip(ldr * dynamic_range, 0, dynamic_range).astype(astype)
+    # 255 65536 4294967296
+    return im2
